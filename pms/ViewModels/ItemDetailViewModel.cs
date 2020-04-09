8c10c994dd211b559fce29d1451f317a92d5f798
qtipee <quent.vallat@gmail.com>
2020-04-09 10:24:30 +0200

    Adds Xamarin project

---
+++ pms/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using pms.Models;
+
+namespace pms.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
