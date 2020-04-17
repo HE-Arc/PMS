16e2170f468f1ff069f37b9e7dfb6cfb8ba7a933
qtipee <quent.vallat@gmail.com>
2020-04-17 16:26:41 +0200

    Take photo + pick photo

--- pms/Views/CameraPage.xaml.cs
+++ pms/Views/CameraPage.xaml.cs
@@ -1,7 +1,11 @@
 ﻿using System;
 using System.ComponentModel;
+using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using Xamarin.Essentials;
+using Plugin.Media;
+using Plugin.Media.Abstractions;
 
 namespace pms.Views
 {
@@ -13,38 +17,112 @@ namespace pms.Views
         public CameraPage()
         {
             InitializeComponent();
-
-            CameraButton.Clicked += CameraButton_Clicked;
         }
 
-        private async void CameraButton_Clicked(object sender, EventArgs e)
+        // TODO: transfer the photo for analyse
+        void ReadPhoto(MediaFile photo)
         {
-            if (! Plugin.Media.CrossMedia.Current.IsCameraAvailable ||
-                    !Plugin.Media.CrossMedia.Current.IsTakePhotoSupported)
+            if (photo != null)
             {
-                await DisplayAlert("No camera", ":(No camera available.", "OK");
+                PhotoImage.Source = ImageSource.FromStream(() => { return photo.GetStream(); });
+            }
+        }
+
+        // Checks wether the application has the camera permission
+        async Task<bool> CheckCameraPermission()
+        {
+            var status = await Permissions.CheckStatusAsync<Permissions.Camera>();
+
+            if (status != PermissionStatus.Granted)
+            {
+                status = await Permissions.RequestAsync<Permissions.Camera>();
+
+                if (status != PermissionStatus.Granted)
+                {
+                    await DisplayAlert("Camera is required", "You need to add camera permission.", "OK");
+
+                    return false;
+                }
             }
 
-            var file = await Plugin.Media.CrossMedia.Current.TakePhotoAsync(new Plugin.Media.Abstractions.StoreCameraMediaOptions()
-            {
-                Directory = "Sample",
-                Name = "test.jpg"
-            });
+            return true;
+        }
 
-            if (file == null)
+        // Checks wether the application has the storage read permission
+        async Task<bool> CheckStorageReadPermission()
+        {
+            var status = await Permissions.CheckStatusAsync<Permissions.StorageRead>();
+
+            if (status != PermissionStatus.Granted)
             {
-                return;
+                status = await Permissions.RequestAsync<Permissions.StorageRead>();
+
+                if (status != PermissionStatus.Granted)
+                {
+                    await DisplayAlert("Storage Read is required", "You need to add storage read permission.", "OK");
+
+                    return false;
+                }
             }
 
-            await DisplayAlert("File Location", file.Path, "OK");
+            return true;
+        }
 
-            ImageSource.FromStream(() =>
+        // Checks wether the camera plugin is available and supported
+        async Task<bool> CheckCameraPlugin()
+        {
+            if (! CrossMedia.Current.IsCameraAvailable || ! CrossMedia.Current.IsTakePhotoSupported)
             {
-                var stream = file.GetStream();
-                file.Dispose();
+                await DisplayAlert("Camera not supported", "No camera available.", "OK");
+                CameraButton.IsEnabled = false;
 
-                return stream;
-            });
+                return false;
+            }
+
+            return true;
+        }
+
+        // Checks wether the pick photo plugin is available and supported
+        async Task<bool> CheckPickPhotoPlugin()
+        {
+            if (! CrossMedia.Current.IsPickPhotoSupported)
+            {
+                await DisplayAlert("Pick photo not supported", "No media picker available.", "OK");
+                MediaPickButton.IsEnabled = false;
+
+                return false;
+            }
+
+            return true;
+        }
+
+        // Click on Take Photo button
+        async void CameraButton_OnClicked(object sender, EventArgs e)
+		{
+            if (await CheckCameraPermission() && await CheckCameraPlugin())
+            {
+                // Supplies media options for saving the photo after it is taken
+                var mediaOptions = new StoreCameraMediaOptions
+                {
+                    Directory = "Samples",
+                    Name = $"{DateTime.UtcNow}.jpg"
+                };
+
+                // Takes a photo
+                var photo = await CrossMedia.Current.TakePhotoAsync(mediaOptions);
+                ReadPhoto(photo);
+            }
+        }
+
+        // Click on Pick Photo button
+        async void MediaPickButton_OnClicked(object sender, EventArgs e)
+        {
+            // Picks a photo 
+            if (await CheckStorageReadPermission() && await CheckPickPhotoPlugin())
+            {
+                var photo = await CrossMedia.Current.PickPhotoAsync();
+                ReadPhoto(photo);
+            }
         }
     }
 }
